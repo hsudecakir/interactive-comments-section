1d2440ed31c1daa7b50e8e2e9371d14f7b8e0f37
Hacer Sude Çakır <sudecakir_62@hotmail.com>
2024-12-10 17:41:32 +0300

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,20 @@ let currentUser;
 
 async function init() {
   const data = await fetch('assets/json/data.json').then(res => res.json());
-  currentUser = data.currentUser.username;
+  currentUser = data.currentUser;
+  console.log(JSON.parse(localStorage.getItem('comments')));
+  if(JSON.parse(localStorage.getItem('comments')).length !== 0){
+    data = JSON.parse(localStorage.getItem('comments'));
+    replies = JSON.parse(localStorage.getItem('replies'));
+  }
   render(data, currentUser);
-  // comments.push(data);
   for (const comment of data.comments) {
     comments.push(comment);
+    for (const reply of comment.replies) {
+      replies.push(reply);
+    }
   }
-  addCurrentUserClass(data);
+  addCurrentUserClass(currentUser);
 }
 
 init();
@@ -50,98 +57,90 @@ function renderComments(commentDatas, currentUser){
                 <p class="created-at">${comment.createdAt}</p>
               </div>
               <div class="btns">
-                <div class="reply-btn" data-id="${comment.id}">
-                  <i class="fa-solid fa-reply"></i>
-                  <p>Reply</p>
-                </div>
-                <div class="delete-btn" data-id="${comment.id}">
-                  <i class="fa-solid fa-trash"></i>
-                  <p>Delete</p>
-                </div>
-                <div class="edit-btn" data-id="${comment.id}">
-                  <i class="fa-solid fa-pen"></i>
-                  <p>Edit</p>
-                </div>
+                <button class="reply-btn" data-id="${comment.id}">
+                  <i class="fa-solid fa-reply"></i> Reply
+                </button>
+                <button class="delete-btn" data-id="${comment.id}">
+                  <i class="fa-solid fa-trash"></i> Delete
+                </button>
+                <button class="edit-btn" data-id="${comment.id}">
+                  <i class="fa-solid fa-pen"></i> Edit
+                </button>
               </div>
             </div>
             <p class="content">${comment.content}</p>
-            ${currentUser == comment.user.username ? '<textarea class="editTextarea"></textarea><div class="update-btn-container"><button class="update-btn">UPDATE</button></div>' : ''}
+            ${currentUser.username == comment.user.username ? `<textarea class="editTextarea"></textarea><div class="update-btn-container"><button class="update-btn" data-id="${comment.id}">UPDATE</button></div>` : ''}
           </div>
         </div>
-        ${"replies" in comment && comment.replies.length > 0 ? `<div class="reply-container" data-id="${comment.id}"></div>` : ''}
+        <div class="reply-container" data-id="${comment.id}"></div>
       </div>
     `
   }
+  addCurrentUserClass(currentUser);
+  renderReplies(commentDatas, currentUser);
   bindDeleteBtns();
-  const editBtns = document.querySelectorAll('.edit-btn');
-  for (const editBtn of editBtns) {
-    editBtn.addEventListener('click', (e) => editComment(e, commentDatas));
-  } 
+  bindEditBtns(commentDatas); 
+  bindReplyBtns(commentDatas);
+  localStorage.setItem('comments', JSON.stringify(comments));
+  localStorage.setItem('replies', JSON.stringify(replies));
 }
 
 function renderReplies(commentDatas, currentUser){
   const replyContainers = document.querySelectorAll('.reply-container');
   for (const replyContainer of replyContainers) {
-    const repliedComments = commentDatas.comments.find(x => x.id == replyContainer.dataset.id);
+    const repliedComments = commentDatas.find(x => x.id == replyContainer.dataset.id) || replies.find(x => x.id == replyContainer.dataset.id);
     for (const repliedComment of repliedComments.replies) {
       replyContainer.innerHTML += `
-      <div class="reply" data-username="${repliedComment.user.username}">
-      <div class="score">
-      <i class="fa-solid fa-plus"></i>
-      <p>${repliedComment.score}</p>
-      <i class="fa-solid fa-minus"></i>
+      <div class="reply" data-username="${repliedComment.user.username}" data-id="${repliedComment.id}">
+        <div class="score">
+          <i class="fa-solid fa-plus"></i>
+          <p>${repliedComment.score}</p>
+          <i class="fa-solid fa-minus"></i>
+        </div>
+        <div class="reply__wrapper">
+          <div class="reply__wrapper--top">
+            <div class="user-info">
+              <img src="assets${repliedComment.user.image.svg}" alt="Profile Picture">
+              <p class="username">${repliedComment.user.username}</p>
+            <span>you</span>
+            <p class="created-at">${repliedComment.createdAt}</p>
+            </div>
+            <div class="btns">
+              <button class="reply-btn" data-id="${repliedComment.id}">
+                <i class="fa-solid fa-reply"></i> Reply
+              </button>
+              <button class="delete-btn" data-id="${repliedComment.id}">
+                <i class="fa-solid fa-trash"></i> Delete
+              </button>
+              <button class="edit-btn" data-id="${repliedComment.id}">
+                <i class="fa-solid fa-pen"></i> Edit
+              </button>
+            </div>
+          </div>
+          <p class="content">
+            <span class="replied-to">@${repliedComment.replyingTo}</span>
+            ${repliedComment.content}
+          </p>
+            ${currentUser.username == repliedComment.user.username ? `<textarea class="editTextarea"></textarea><div class="update-btn-container"><button class="update-btn" data-id="${repliedComment.id}">UPDATE</button></div>` : ''}
+        </div>
       </div>
-      <div class="reply__wrapper">
-      <div class="reply__wrapper--top">
-      <div class="user-info">
-      <img src="assets${repliedComment.user.image.svg}" alt="Profile Picture">
-      <p class="username">${repliedComment.user.username}</p>
-      <span>you</span>
-      <p class="created-at">${repliedComment.createdAt}</p>
-      </div>
-      <div class="btns">
-      <div class="reply-btn" data-id="${repliedComment.id}">
-      <i class="fa-solid fa-reply"></i>
-      <p>Reply</p>
-      </div>
-      <div class="delete-btn" data-id="${repliedComment.id}">
-      <i class="fa-solid fa-trash"></i>
-      <p>Delete</p>
-      </div>
-      <div class="edit-btn" data-id="${repliedComment.id}">
-      <i class="fa-solid fa-pen"></i>
-      <p>Edit</p>
-      </div>
-      </div>
-      </div>
-      <p class="content">
-      <span class="replied-to">@${repliedComment.replyingTo}</span>
-      ${repliedComment.content}
-      </p>
-      </div>
-      </div>
+      <div class="reply-container" data-id="${repliedComment.id}"></div>
       `
     }
   }
-  addCurrentUserClass(commentDatas);
-  bindDeleteBtns();
-  const editBtns = document.querySelectorAll('.edit-btn');
-  for (const editBtn of editBtns) {
-    editBtn.addEventListener('click', (e) => editComment(e, commentDatas));
-  }
+  addCurrentUserClass(currentUser);
 }
 
-function addCurrentUserClass(commentDatas){
-  currentUser = commentDatas.currentUser.username;
+function addCurrentUserClass(currentUser){
   const replies = document.querySelectorAll('.reply');
   const comments = document.querySelectorAll('.comment');
   for (const reply of replies) {
-    if(reply.dataset.username == currentUser){
+    if(reply.dataset.username == currentUser.username){
       reply.classList.add('current-user');
     }
   }
   for (const comment of comments) {
-    if(comment.dataset.username == currentUser){
+    if(comment.dataset.username == currentUser.username){
       comment.classList.add('current-user');
     }
   }
@@ -149,12 +148,17 @@ function addCurrentUserClass(commentDatas){
 
 function addComment(e, commentDatas){
   for (const comment of comments) {
-    replies.push(comment.replies);
+    for (const reply of comment.replies) {
+      replies.push(reply);
+    }
   }
+  console.log(replies)
+  const lastComment = comments[comments.length - 1];
+  const lastReply = replies[replies.length - 1];
   if(addCommentTextarea.value.trim() !== ''){
     comments.push(
       {
-        id: comments.length + replies.length + 1,
+        id: lastComment.id > lastReply.id ? lastComment.id + 1 : lastReply.id + 1,
         content: `${addCommentTextarea.value}`,
         createdAt: "Just now",
         score: 0,
@@ -164,7 +168,7 @@ function addComment(e, commentDatas){
             svg: `/images/${commentDatas.currentUser.username}.svg`,
             webp: `/images/${commentDatas.currentUser.username}.webp`
           },
-          username: `${commentDatas.currentUser.username}`
+          username: `${commentDatas.currentUser.username}`,
         },
         replies: []
       }
@@ -172,7 +176,6 @@ function addComment(e, commentDatas){
     addCommentTextarea.value = '';
   }
   renderComments(comments, currentUser);
-  renderReplies(commentDatas);
 }
 
 function bindDeleteBtns(){
@@ -189,36 +192,112 @@ function bindEditBtns(commentDatas){
   }
 }
 
+function bindReplyBtns(commentDatas, currentUser){
+  const replyBtns = document.querySelectorAll('.reply-btn');
+  for (const replyBtn of replyBtns) {
+    replyBtn.addEventListener('click', (e) => replyComment(e, commentDatas));
+  }
+}
+
 function deleteComment(){
   const selectedComment = comments.findIndex(x => x.id == this.dataset.id);
-  comments.splice(selectedComment, 1);
+  if(selectedComment !== -1){
+    comments.splice(selectedComment, 1);
+  }
   const selectedReply = replies.findIndex(x => x.id == this.dataset.id);
-  replies.splice(selectedReply, 1);
-  // renderComments(comments.comments);
-  console.log(comments);
-  // renderReplies(comments);
+  if(selectedReply !== -1){
+    for (const comment of comments) {
+      const parentComment = comment.replies.find(x => x.id == this.dataset.id);
+        if(parentComment){
+          comment.replies.splice(selectedReply, 1);
+        }
+    }
+    replies.splice(selectedReply, 1);
+
+  }
+  renderComments(comments, currentUser);
 }
 
 function editComment(e, commentDatas){
   const currentComments = document.querySelectorAll('.comment');
   for (const comment of currentComments) {
-    console.log(currentComments);
     if(e.target.dataset.id == comment.dataset.id){
-      comment.classList.add('editing');
-      const content = document.querySelector('.editing .content');
-      const updateBtn = document.querySelector('.update-btn');
+      comment.classList.toggle('editing');
+      const content = document.querySelector(`.editing[data-id="${e.target.dataset.id}"] .content`);
+      const updateBtn = document.querySelector(`.update-btn[data-id="${e.target.dataset.id}"]`);
+      const textarea = document.querySelector(`.editing[data-id="${e.target.dataset.id}"] .editTextarea`);
+      textarea.focus();
       updateBtn.addEventListener('click', () => {
-        console.log(comment);
-        const textarea = document.querySelector('.editing .editTextarea')
         comment.classList.remove('editing');
         const updatedComment = commentDatas.find(x => x.id == comment.dataset.id);
         updatedComment.content = textarea.value;
         content.innerText = textarea.value;
       })
-      return;
     }
-    
   }
-  console.log(comments);
+  const currentReplyContainers = document.querySelectorAll('.reply');
+  for (const reply of currentReplyContainers) {
+    if(e.target.dataset.id == reply.dataset.id){
+      reply.classList.toggle('editing');
+      const content = document.querySelector(`.editing[data-id="${e.target.dataset.id}"] .content`);
+      const updateBtn = document.querySelector(`.update-btn[data-id="${e.target.dataset.id}"]`);
+      const textarea = document.querySelector(`.editing[data-id="${e.target.dataset.id}"] .editTextarea`);
+      const updatedReply = replies.find(x => x.id == reply.dataset.id);
+      textarea.focus();
+      updateBtn.addEventListener('click', () => {
+        reply.classList.remove('editing');
+        updatedReply.content = textarea.value;
+        content.innerHTML = `<span class="replied-to">@${updatedReply.replyingTo}</span> ${textarea.value}`;
+      })
+    }
+  }
+}
+
+function replyComment(e, commentDatas){
+  const repliedComment = document.querySelector(`.reply-container[data-id="${e.target.dataset.id}"]`);
+  const repliedCommentId = commentDatas.find(x => x.id == e.target.dataset.id) || replies.find(x => x.id == e.target.dataset.id);
+  repliedComment.innerHTML += `
+    <div class="reply">
+      <img src="assets${currentUser.image.svg}" alt="Profile Picture">
+      <textarea class="reply-textarea" data-id="${repliedCommentId.id}"></textarea>
+      <button class="new-reply-btn" data-id="${repliedCommentId.id}">REPLY</button>
+    </div>
+  `
+  const sendReplyBtn = document.querySelector(`.new-reply-btn[data-id="${repliedCommentId.id}"]`);
+  const sendReplyTextarea = document.querySelector(`.reply-textarea[data-id="${repliedCommentId.id}"]`);
+  sendReplyBtn.addEventListener('click', () => {
+    const lastComment = comments[comments.length - 1];
+    const lastReply = replies[replies.length - 1];
+    const newReply = {
+      id: lastComment.id > lastReply.id ? lastComment.id + 1 : lastReply.id + 1,
+      content: `${sendReplyTextarea.value}`,
+      createdAt: "Just Now",
+      score: 0,
+      replyingTo: `${repliedCommentId.user.username}`,
+      user: {
+        image: { 
+          png: `/images/${currentUser.username}.png`,
+          svg: `/images/${currentUser.username}.svg`,
+          webp: `/images/${currentUser.username}.webp`
+        },
+        username: `${currentUser.username}`
+      }
+    }
+    // repliedCommentId.replies.push(newReply);
+    // replies.push(newReply);
+    for (const comment of commentDatas) {
+      if(comment.id === repliedCommentId.id){
+        repliedCommentId.replies.push(newReply);
+        replies.push(newReply);
+      } else{
+        const parentComment = comment.replies.find(x => x.id == repliedCommentId.id);
+        if(parentComment){
+          comment.replies.push(newReply);
+          replies.push(newReply);
+        }
+      }
+    }
+    renderComments(comments, currentUser);
+  });
 }
 
